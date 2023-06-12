7ba2eaf447e23dfc93cb852ca1fd597bf3bfacc9
Joseph Ukwenya <josephukwenya.ng@gmail.com>
2023-06-12 15:39:57 +0100

    added dotenv package for storing secrets

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const morgan = require('morgan');
 const cors = require('cors');
 const app = express();
 const PORT = 5000; 
 
 app.use(cors());
+app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
