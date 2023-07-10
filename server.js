04eb4f55e4a818890fcf0e72dc16348c11b84ae1
Joseph Ukwenya NG <programmingwithjoseph.ng@gmail.com>
2023-07-10 18:38:13 +0100

    updated port

--- server.js
+++ server.js
@@ -1,31 +1,26 @@
-const express = require("express");
-const morgan = require("morgan");
-const cors = require("cors");
-require("dotenv").config();
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+require('dotenv').config();
 
 const app = express();
 
-const authRouter = require("./routes/authRoutes");
-const todoRouter = require("./routes/todoRoutes");
+const authRouter = require('./routes/authRoutes');
+const todoRouter = require('./routes/todoRoutes');
 
 const PORT = process.env.PORT || 5000;
 
 // Middlewares
-if (process.env.NODE_ENV === "development") {
-  app.use(morgan("dev"));
-}
-
+app.use(morgan('dev'));
 app.use(express.json());
-app.use(cors({ origin: "https://appacademy-todoapp.vercel.app/" }));
+app.use(cors({ origin: 'https://appacademy-todoapp.vercel.app/' }));
 app.use(express.urlencoded({ extended: false }));
 
-app.get("/", (req, res) => {
-  res.json("App Academy Todo Web App!!!!");
+app.get('/', (req, res) => {
+  res.json('App Academy Todo Web App!!!!');
 });
 
-app.use("/api/v1/users", authRouter);
-app.use("/api/v1/todos", todoRouter);
+app.use('/api/v1/users', authRouter);
+app.use('/api/v1/todos', todoRouter);
 
-app.listen(PORT, () =>
-  console.log(`Server running in ${process.env.NODE_ENV} mode on PORT ${PORT}`)
-);
+app.listen(PORT, () => console.log(`Server running on PORT ${PORT}`));
