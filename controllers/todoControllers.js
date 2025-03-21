39cd0564bd17514dc0a4c54fb9658502ca222208
Joseph Ukwenya <josephukwenya.ng@gmail.com>
2025-03-21 16:28:54 +0100

    revamp to use prisma

--- controllers/todoControllers.js
+++ controllers/todoControllers.js
@@ -1,46 +1,67 @@
-const Todo = require("../models/Todo");
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
 
 exports.getTodos = async (req, res) => {
   const userId = req.user.id;
   try {
-    const todos = await Todo.findAll({ where: { userId } });
+    const todos = await prisma.todo.findMany({
+      where: { userId },
+    });
     return res.json(todos);
   } catch (err) {
     console.log(err);
-    return "Error";
+    return res.status(500).json({ error: 'Internal server error' });
   }
 };
 
 exports.createTodo = async (req, res) => {
   console.log(req.user);
   try {
-    const newTodo = new Todo({
-      userId: req.user.id,
-      task: req.body.task,
+    const newTodo = await prisma.todo.create({
+      data: {
+        userId: req.user.id,
+        task: req.body.task,
+      },
     });
-
-    newTodo.save();
     return res.status(201).json(newTodo);
   } catch (err) {
     console.error(err);
-    return "Something went wrong";
+    return res.status(500).json({ error: 'Internal server error' });
   }
 };
 
 exports.toggleTodo = async (req, res) => {
   const { id } = req.params;
-  const todo = await Todo.findOne({ where: { id } });
+  try {
+    const todo = await prisma.todo.findUnique({
+      where: { id },
+    });
 
-  todo.complete = !todo.complete;
+    if (!todo) {
+      return res.status(404).json({ error: 'Todo not found' });
+    }
 
-  todo.save();
+    const updatedTodo = await prisma.todo.update({
+      where: { id },
+      data: { complete: !todo.complete },
+    });
 
-  res.json(todo);
+    res.json(updatedTodo);
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({ error: 'Internal server error' });
+  }
 };
 
 exports.deleteTodo = async (req, res) => {
-  const id = req.params.id;
-  const todo = await Todo.destroy({ where: { id } });
-
-  res.status(204).json(todo);
+  const { id } = req.params;
+  try {
+    await prisma.todo.delete({
+      where: { id },
+    });
+    res.status(204).json({ message: 'Todo deleted successfully' });
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({ error: 'Internal server error' });
+  }
 };
