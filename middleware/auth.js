5eb9add0e74d9ab096e7ad85a33a27b44835636d
Joseph Ukwenya <josephukwenya.ng@gmail.com>
2025-03-21 16:46:25 +0100

    fix: middleware function

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,12 +1,17 @@
-const jwt = require("jsonwebtoken");
-require("dotenv").config();
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
 
 module.exports = function (req, res, next) {
-  const token = req.header("Authorization");
+  let token = req.header('Authorization');
   console.log({ token });
 
   if (!token) {
-    return res.status(403).json({ msg: "authorization denied" });
+    return res.status(403).json({ msg: 'Authorization denied' });
+  }
+
+  // Remove "Bearer " prefix if present
+  if (token.startsWith('Bearer ')) {
+    token = token.slice(7, token.length).trimLeft();
   }
 
   try {
@@ -15,6 +20,6 @@ module.exports = function (req, res, next) {
     req.user = verify.user;
     next();
   } catch (err) {
-    res.status(401).json({ msg: "Token is not valid" });
+    res.status(401).json({ msg: 'Token is not valid' });
   }
 };
